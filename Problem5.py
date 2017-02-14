2ee73f237e3e23a46a5f76fe1aa1e05c3f102536
Eddy Yao <eddy2yao2@gmail.com>
2017-02-14 14:10:43 -0500

    Update Problem5.py

--- Problem5.py
+++ Problem5.py
@@ -1,1 +1,3 @@
 print(2 * 2 * 2 * 2 * 3 * 3 * 5 * 7 * 11 * 13 * 17 * 19)
+# The solution will be the product of all primes below or equal to 20, with any prime who's square (or any other power) in the set [1, 20]
+# counted multiple times. This is true due to the fundamental theorem of arithmetic.
